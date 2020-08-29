dc94bb1aea51e9075d2cc6d54f3d53a116c9d68f
abhay304 <abhaykumar30495@gmail.com>
2020-08-30 02:39:18 +0530

    make it better

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl :"https://post-mean-server.herokuapp.com/api/"
+};
+// Aws :- http://postwebapp-env.eba-fygmveek.us-east-2.elasticbeanstalk.com/api/
